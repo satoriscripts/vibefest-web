5e2da548a8bb7d9404923b7763ec359391895bba
matt <30363562+mmattbtw@users.noreply.github.com>
2022-11-15 17:37:46 -0600

    Update index.tsx

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -62,6 +62,9 @@ export default function IndexPage() {
                 BUY THE CHARITY MERCH (Radio Vibefest.)
               </a>
             </p>
+            
+            <h4>Radio Vibefest.</h4>
+            <iframe src="https://radio.vibefest.live/public/radio_vibefest/embed?theme=dark" frameborder="0" allowtransparency="true" style="width: 100%; min-height: 150px; border: 0;"></iframe>
           </div>
         </Spring>
       </div>
